d71c41fa64affdd82bd2fcf5b8538be0b6d523c3
“Sam <smans123@gmail.com>
2021-01-29 07:28:29 -0500

    Adding Form compponent for inputting and submitting to do items. Adding interface to handle setting state data to fix typescript errors and adding events and methods to handle submitting data

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,50 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const Hello = () => {
-  return (
-    <h2>Hello World!</h2>
-  );
-}
+interface ToDoState {
+  value: string;
+} // Defining type to avoid TypeScript errors when accessing State in component render functions. Explanation: https://stackoverflow.com/questions/47561848/property-value-does-not-exist-on-type-readonly
 
 const Title = () => {
   return (
     <h1>To Do List Application</h1>
   );
+}
+
+class ToDoForm extends React.Component <{}, ToDoState> {
+  
+
+  constructor(props: any, state:ToDoState) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event: any) {
+    const changeValue = event.target.value;
+    this.setState({value: changeValue});
+  }
+
+  handleSubmit(event: any) {
+    event.preventDefault();
+    const submitValue = this.state.value;
+    console.log("Submitting Value:", submitValue);
+  }
+
+  render() {
+    const stateValue: string = this.state.value;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          To Do Item:
+          <input type="text" value={stateValue} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
 }
 
 function App() {
@@ -21,7 +55,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
       </header>
       <Title />
-      <Hello />
+      <ToDoForm />
     </div>
   );
 }
